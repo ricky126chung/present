5a96c74f51a9b005cb80b4b1c15a11a4afd685e0
AUTOMATED\rickychung <rickychung@asl.com.hk>
2025-06-10 15:38:57 +0800

    Add react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
-
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
+import React, { useMemo } from 'react';
+import Home from './pages/Home';
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        {/* <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a> */}
-      </header>
-    </div>
-  );
+  
+  
+    const routeList = useMemo(() => [
+      {
+        path: "/present",
+        component: <Home />,
+      }
+    ], []);
+    
+    
+    return (
+      <BrowserRouter>
+        <Routes>
+          {routeList.map((route) => (
+            <Route 
+              key={route.path} 
+              path={route.path} 
+              element={route.component} 
+            />
+          ))}
+        </Routes>
+      </BrowserRouter>
+    );
 }
 
 export default App;
